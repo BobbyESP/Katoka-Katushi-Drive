7524425d2a80b9784883ab8cf92d35fdb4547f80
BobbyESP <gabilessto@gmail.com>
2021-06-08 16:47:56 +0200

    Primer Commit con todos los archivos.

---
+++ src/components/google-drive/Navbar.js
@@ -0,0 +1,27 @@
+import React from "react"
+import { Navbar, Nav } from "react-bootstrap"
+import { Link } from "react-router-dom"
+
+
+export default function NavbarComponent() {
+  return (
+    <Navbar bg="light" expand="sm">
+      <Navbar.Brand as={Link} to="">
+        Katushi & Katoka Drive
+      </Navbar.Brand>
+      <Nav>
+        <Nav.Link as={Link} to="/user">
+          Perfil
+        </Nav.Link>
+        <Nav.Link as={Link}>
+        <a href="https://console.firebase.google.com/u/0/project/drive-personal-865ae/overview">
+          Consola Firebase (Desarrollo)
+        </a>
+        </Nav.Link>
+        <Nav.Link as={Link} to="/testApp">
+          FILE PREVIEWING TEST
+        </Nav.Link>
+      </Nav>
+    </Navbar>
+  )
+}
